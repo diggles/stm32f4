6f5a20371b026f4b4df92b90296f1c2c8ef06ef4
diggles <sypher.mapping@gmail.com>
2015-04-11 17:21:05 +1200

    Initial LED blink test

---
+++ LEDTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using System.Threading;
+using Microsoft.SPOT.Hardware.STM32F4;
+
+namespace STM32F4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            OutputPort led = new OutputPort(Pins.GPIO_PIN_D_14, false); //PA1 on discovery board
+
+            while (true)
+            {
+                led.Write(true);
+                Thread.Sleep(500);
+                led.Write(false);
+                Thread.Sleep(500);
+            }
+        }
+
+    }
+}
